11ec2d80ff658a503659abd716661d540b66df0c
Sizov <ser-reb@yandex.ru>
2023-07-12 16:01:37 +0400

    Заготовка для функции отображения помощи

--- scripts.py
+++ scripts.py
@@ -38,7 +38,6 @@ def generator_wins(dim: int) -> list:
 def show_title():
     """Выводит заголовок игры"""
         
-#    turtles = get_terminal_size()
     width = get_terminal_size().columns - 2
 
     title = f'\n#{"="*(width)}##{" "*(width)}##'    
@@ -47,3 +46,16 @@ def show_title():
     title += f'##{" "*(width)}##{"="*(width)}#\n'
     
     print(title)
+    
+def show_help():
+    """Выводит справку по игре"""  
+
+    width = get_terminal_size().columns
+    
+    text_help = f'{"СПРАВКА".center(width)}\n'
+    text_help += '  Приложение с интерфейсом командной строки, с помощью которого можно играть одному или вдвоём в игру крестики-нолики на квадратном поле.\n'
+    
+    text_help += '  После запуска приложение входит в главное меню, в котором ожидает ввод команд игрока. С помощью команд можно настраивать игру, запускать новую партию и выполнять другие действия.\n'
+    
+
+    print(text_help)
