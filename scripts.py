a4c0912811a93ce346c3598daad859165c285585
Gennadiy <renardes@gmail.com>
2023-07-12 14:28:02 +0500

    правки удобства чтения кода

--- scripts.py
+++ scripts.py
@@ -1,16 +1,20 @@
 from shutil import get_terminal_size 
 
+
 # размер поля
 dim = 3
+# ИСПОЛЬЗОВАТЬ: в all_cells лучше записать общее число ячеек (возможных ходов)
+all_cells = dim**2
 
-# список ходов 
+# КОММЕНТАРИЙ: такой список вряд ли понадобится
+empty_row = [' ']*dim
+
+# список ходов
 turns = []
 
-all_cells = [' ']*dim
 
 def generator_board(dim: int) -> str:
     """Генерирует шаблон игрового поля для отображения"""
-    
     out = ''
     for _ in range(dim-1):
         out += ' | '.join(['{}']*dim)
@@ -18,32 +22,32 @@ def generator_board(dim: int) -> str:
     out += ' | '.join(['{}']*dim)
      
     return out 
-    
+
+
 def generator_wins(dim: int) -> list:
     """Генерирует выигрышные комбинации"""  
-
     out = []
-    myList = [i for i in range(0, dim**2)]
+    # ИСПОЛЬЗОВАТЬ: регистр lower_snake_case для всех имён кроме классов и условных констант
+    my_list = [i for i in range(0, dim**2)]
     
     for i in range(dim):
-        out.append({element for element in myList[i*dim:dim*(i+1)]})
-        out.append({element for element in myList[i::dim]})
-        
-    out.append({element for element in myList[::dim+1]})    
-    out.append({element for element in myList[dim-1:-1:dim-1]})
-#    print(out) 
+        out.append({element for element in my_list[i*dim:dim*(i+1)]})
+        out.append({element for element in my_list[i::dim]})
+    out.append({element for element in my_list[::dim+1]})
+    out.append({element for element in my_list[dim-1:-1:dim-1]})
+    # print(out)
     return out
     
     
 def show_title():
     """Выводит заголовок игры"""
-        
-#    turtles = get_terminal_size()
+    # turtles = get_terminal_size()
     width = get_terminal_size().columns - 2
 
-    title = f'\n#{"="*(width)}##{" "*(width)}##'    
+    title = f'\n#{"="*width}##{" "*width}##'
     title += f'{"Игра".center(width)}##'  
     title += f'{"Крестики-Нолики".center(width)}' 
-    title += f'##{" "*(width)}##{"="*(width)}#\n'
+    title += f'##{" "*width}##{"="*width}#\n'
     
     print(title)
+
