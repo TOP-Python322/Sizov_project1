abf60b0f5025d48c8b20008697ba2f18463f143b
Sizov <ser-reb@yandex.ru>
2023-07-14 12:26:23 +0400

    добавление функции для чтения и записи статистики

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,30 @@
 """
 Вспомогательный модуль: вспомогательные функции.
 """
+
+# стандартная библиотека
+from configparser import ConfigParser
+# проект
+import data
+
+
+def read_ini() -> dict:
+    """Читает данные из файла и формирует структуру данных по статистике"""    
+    config = ConfigParser()    
+    config.read(data.PLAYERS_DB_PATH)
+    for player in config.sections(): 
+        data.players_db[player] = {
+            k: int(v)
+            for k, v in config[player].items()
+        }
+    return bool(data.players_db)
+ 
+ 
+def write_ini() -> None:
+    """Записывает структуру данных по статистике в файл"""
+    config = ConfigParser()
+    config.read_dict(data.players_db)
+    with open(data.PLAYERS_DB_PATH, 'w', encoding='utf-8') as configfile:
+        config.write(configfile) 
+        
+
