e6469ca4eeab13808b0906fc12e5e6b0a54e7a88
Sizov <ser-reb@yandex.ru>
2023-07-14 12:31:01 +0400

    начало игры

--- src/main.py
+++ src/main.py
@@ -8,3 +8,15 @@ import game
 import help
 import players
 import utils
+
+# выводим заставку
+help.show_title()
+
+# Чтение файлов данных
+# ЕСЛИ первый запуск:
+if not utils.read_ini():
+    # вывод раздела помощи 
+    help.show_help()
+    
+# Запрос имени игрока
+players.get_name()    
