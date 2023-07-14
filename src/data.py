aed8347af534fd90fd19a1c0c5753a9fc767b72d
Sizov <ser-reb@yandex.ru>
2023-07-14 12:30:17 +0400

    добавление переменных и условных констант

--- src/data.py
+++ src/data.py
@@ -2,6 +2,24 @@
 Вспомогательный модуль: глобальные переменные и условные константы.
 """
 
+# стандартная библиотека
+from pathlib import Path
+from re import compile
+from sys import path
+
+import utils
+
+
+ROOT_DIR = Path(path[0]).parent
+PLAYERS_DB_PATH = ROOT_DIR / 'data/statistics.ini'
+SAVES_DB_PATH = ROOT_DIR / 'data/saves.txt'
+
+name_pattern = compile(r'[a-zA-Zа-яА-Я][а-яА-Я\w]+')
+
+# база игроков - имена и статистика игроков 
+players_db: dict[str, dict[str, int]] = {}
+
+
 COMMANDS = {
     'помощь': ('help', 'помощь'),
     'новая партия': ('new', 'игра'),
@@ -11,3 +29,21 @@ COMMANDS = {
     'размер поля': ('dim', 'размер'),
     'выход': ('quit', 'выход'),    
 }
+MESSAGES = {
+    'ввод команды': '\n > введите команду: ',
+    'ввод имени': '\n > введите имя: ',
+    'некорректное имя': ' ! имя игрока должно начинаться с буквы и быть не короче двух символов',
+    # '': '',
+}
+
+
+authorized_player: str = None
+active_players: list[str] = []
+
+# размер поля
+dim: int = 3
+
+dim_range: range = range(dim)
+all_cells: int = dim**2
+
+field_template: str = None
