285ca168349c96e6db849f7e015a39e3fb1695c1
Sizov <ser-reb@yandex.ru>
2023-07-16 22:35:48 +0400

    устранение ошибки в генерации хода бота

--- src/bot.py
+++ src/bot.py
@@ -11,7 +11,7 @@ def game_low() -> int:
     """ Генерирует ход для легкого бота"""
     # пока так, расмотреть другие варианты
     while True:
-        step_bot = randint(0, data.all_cells)
+        step_bot = randint(0, data.all_cells-1)
         if step_bot not in data.turns:
             break
             
